6925f903415772769e9fe78ae5ba8a0252e71f96
the.imany <giltyabsolutely@gmail.com>
2022-04-21 13:12:15 +0500

    strugling with appearing menu

--- src/js/basket/basket.js
+++ src/js/basket/basket.js
@@ -5,6 +5,36 @@ export default basket = {
    quantity: 0,
    summ: 0,
    products: [],
-
-
+   add(productId) {
+      let existProduct = this.products.find(item => item.id == productId);
+      if (!existProduct) {
+          this.products.push({
+              id: productId,
+              quantity: 1
+          })
+      } else {
+          existProduct.quantity++;
+      }
+      this._calculateQuantity();
+      this._calculateSumm();
+      console.log(this.products);
+  },
+  _calculateQuantity() {
+      this.quantity = 0;
+      this.products.forEach(product => {
+          this.quantity += product.quantity
+      })
+      document.querySelector('.goods_amount').innerHTML = this.quantity;
+  },
+  _calculateSumm() {
+      this.summ = 0;
+      this.products.forEach(product => {
+          let databaseProduct = products.find(item => product.id == item.id);
+          if (databaseProduct) {
+              this.summ += parseInt(product.quantity) * parseFloat(databaseProduct.price); 
+          }
+      })
+      document.querySelector('.price-amount').innerHTML = this.summ;
+  }
 }
+
--- src/js/main.js
+++ src/js/main.js
@@ -2,18 +2,18 @@ import '../styles/main.scss';
 import { readJson } from './api.js';
 import products from './products/products.js';
 import products_rendering from './products/products_rendering.js';
-
-
+import basket from './basket/basket.js'
 
 
 
 readJson();
 products_rendering();
 
-let basketAddButton = document.querySelector ('.Price').forEach(item => {
-  item.addEventListener ('click', function (e) {
-      basket.add (e.target.getAttribute('product_id'));
+let basketAddButton = document.querySelectorAll('.order_details').forEach(item => {
+  item.addEventListener('click', function (e) {
+    basket.add(e.target.getAttribute('product_id'));
+  })
 
-  }
-  )
-}); 
+});
+
+
